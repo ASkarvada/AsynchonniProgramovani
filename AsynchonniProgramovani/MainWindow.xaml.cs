c6d46dc0fc5a38eda715ba7b29b5623b663293b4
Adam Škarvada <adam.skarvada@seznam.cz>
2021-01-22 01:13:46 +0100

    Přidejte soubory projektu.

---
+++ AsynchonniProgramovani/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsynchonniProgramovani
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ThreadPool tp;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            tp = new ThreadPool();
+
+            condition1.Items.Add("Obsahuje čísla 5,2,1");
+            condition1.Items.Add("Obsahuje 2 stejná čísla");
+
+            condition2.Items.Add("Obsahuje čísla 5,2,1");
+            condition2.Items.Add("Obsahuje 2 stejná čísla");
+
+            condition3.Items.Add("Obsahuje čísla 5,2,1");
+            condition3.Items.Add("Obsahuje 2 stejná čísla");
+
+            condition4.Items.Add("Obsahuje čísla 5,2,1");
+            condition4.Items.Add("Obsahuje 2 stejná čísla");
+        }
+
+        private async void b1_Click(object sender, RoutedEventArgs e)
+        {
+            int selected = condition1.SelectedIndex;
+            string number;
+            b1.IsEnabled = false;
+            result1.Text = "Probíhá výpočet...";
+            number = await tp.SearchAsync(selected);
+            b1.IsEnabled = true;
+            result1.Text = number;
+        }
+
+        private async void b2_Click(object sender, RoutedEventArgs e)
+        {
+            int selected = condition2.SelectedIndex;
+            string number;
+            b2.IsEnabled = false;
+            result2.Text = "Probíhá výpočet...";
+            number = await tp.SearchAsync(selected);
+            b2.IsEnabled = true;
+            result2.Text = number;
+        }
+
+        private async void b3_Click(object sender, RoutedEventArgs e)
+        {
+            int selected = condition3.SelectedIndex;
+            string number;
+            b3.IsEnabled = false;
+            result3.Text = "Probíhá výpočet...";
+            number = await tp.SearchAsync(selected);
+            result3.Text = number;
+            b3.IsEnabled = true;
+        }
+
+        private async void b4_Click(object sender, RoutedEventArgs e)
+        {
+            int selected = condition4.SelectedIndex;
+            string number;
+            b4.IsEnabled = false;
+            result4.Text = "Probíhá výpočet...";
+            number = await tp.SearchAsync(selected);
+            result4.Text = number;
+            b4.IsEnabled = true;
+        }
+    }
+}
---
+++ AsynchonniProgramovani/ThreadPool.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AsynchonniProgramovani
+{
+    public delegate bool Condition(string input);
+    class ThreadPool
+    {
+        
+        public Condition[] CondArr;
+        Condition condition;
+
+        public ThreadPool()
+        {
+            CondArr = new Condition[]
+            {
+                ContainsNumbersFiveTwoOne, ContainsTwoSameNumbers
+            };
+        }
+
+        public bool IsPrime(int input)
+        {
+            int a = 0;
+            for (int i = 1; i <= input; i++)
+            {
+                if (input % i == 0)
+                {
+                    a++;
+                }
+            }
+            if (a == 2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool ContainsTwoSameNumbers(string input)
+        {
+            char firstNumber = ' ';
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input.Length < 2) return false;
+                else
+                {
+                    if(i == 0)
+                    {
+                        firstNumber = input[i];
+                    }
+                    else
+                    {
+                        if (input[i] != firstNumber) return false;
+                        else
+                        {
+                            if ((input.Length - 1) == i) return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool ContainsNumbersFiveTwoOne(string input)
+        {
+            bool five = false;
+            bool two = false;
+            bool one = false;
+            foreach (char item in input)
+            {
+                if (item == '5') five = true;
+                else if (item == '2') two = true;
+                else if (item == '1') one = true;
+            }
+            if (five == true && two == true && one == true) return true;
+            else return false;
+        }
+
+        public async Task<string> SearchAsync(int c)
+        {
+            condition = CondArr[c];
+            int result = 0;
+            await Task.Run(() =>
+            {
+                for (int i = 0; i <= int.MaxValue; i++)
+                {
+                    try
+                    {
+                        if (IsPrime(i) && condition(i.ToString()))
+                        {
+                            result = i;
+                            break;
+                        }
+                    }
+                    catch (Exception err)
+                    {
+                        MessageBox.Show(err.Message);
+                        return;
+                    }
+                }
+            });
+
+            return result.ToString();
+
+        }
+
+
+
+    }
+}
